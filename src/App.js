e9579f07efbb01248194f2d898d574e39d1bc0f3
Master <masterzarby@gmail.com>
2024-11-23 12:17:04 -0400

    Header diminuido usando Componentes, iremos atualizar os css para nao ficar muito pesado para p App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import Logo from "./componentes/logo/index";
-import OpcoesHeader from "./componentes/OpcoesHeader";
 
+import Header from "./componentes/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Logo />
-        <OpcoesHeader />
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/componentes/Header/index.js
@@ -0,0 +1,13 @@
+import Logo from "../logo";
+import OpcoesHeader from "../OpcoesHeader";
+
+function Header() {
+  return (
+    <header className="App-header">
+      <Logo />
+      <OpcoesHeader />
+    </header>
+  );
+}
+
+export default Header;
